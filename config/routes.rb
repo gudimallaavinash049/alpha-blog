636da7152f57d7669743e062a8f12222b1c26d74
gudimallaavinash049 <gudimallaavinash049@gmail.com>
2019-07-24 12:42:30 +0530

    Ready to deploy

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'articles/home', to: 'articles#home'
+
+get 'articles/about', to: 'articles#about'
   resources:articles
 end
