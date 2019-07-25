41061ff0d20cbffa1457146f7cfa08bd9eb00218
gudimallaavinash049 <gudimallaavinash049@gmail.com>
2019-07-25 14:29:12 +0530

    updated

--- Gemfile
+++ Gemfile
@@ -2,6 +2,8 @@ source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.4.6'
+gem 'ruby-debug-ide', '~> 0.7.0'
+gem 'debase', '~> 0.2.3'
 gem 'jquery-rails'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.3'
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -47,23 +47,27 @@ class ArticlesController<ApplicationController
       
       end
 
-   def create
+      def create
 
-      @article = Article.new(article_params)
-      
-      if @article.save
-      
+         @article = Article.new(article_params)
+         
+         @article.user = User.first
+         
+         if @article.save
+         
          flash[:success] = "Article was successfully created"
-      
-      redirect_to article_path(@article)
-      
-      else
-      
-      render 'new'
-      
-      end
-      
-      end
+         
+         redirect_to article_path(@article)
+         
+         else
+         
+         render 'new'
+         
+         end
+         
+         end
+         
+         
       private
       def set_article
       @article=Article.find(params[:id])
