c5bc7531c18c6b05ea6be946266af59babfef16b
gudimallaavinash049 <gudimallaavinash049@gmail.com>
2019-07-25 11:08:14 +0530

    user branch

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_23_055353) do
+ActiveRecord::Schema.define(version: 2019_07_25_051359) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
   end
 
 end
